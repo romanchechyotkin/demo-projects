a3a8cf98ce740c337dd661500b7bc2ad77f19328
Roman Chechyotkin <romanchechyotkin@gmail.com>
2024-12-07 18:12:36 +0300

    hdfs

--- go_hadoop/go.mod
+++ go_hadoop/go.mod
@@ -3,9 +3,13 @@ module hadoop
 go 1.23.0
 
 require (
+	github.com/apache/arrow/go/arrow v0.0.0-20200730104253-651201b0f516 // indirect
+	github.com/apache/thrift v0.14.2 // indirect
 	github.com/colinmarc/hdfs v1.1.3 // indirect
 	github.com/colinmarc/hdfs/v2 v2.4.0 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
 	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
 	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
@@ -13,7 +17,12 @@ require (
 	github.com/jcmturner/goidentity/v6 v6.0.1 // indirect
 	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.13.1 // indirect
+	github.com/pierrec/lz4/v4 v4.1.8 // indirect
+	github.com/xitongsys/parquet-go v1.6.2 // indirect
+	github.com/xitongsys/parquet-go-source v0.0.0-20200817004010-026bad9b25d0 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 )
--- go_hadoop/main.go
+++ go_hadoop/main.go
@@ -1,70 +1,117 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"encoding/json"
+	"io"
 	"log"
+	"math"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
 
-	"github.com/colinmarc/hdfs/v2"
+	"github.com/google/uuid"
+	"github.com/xitongsys/parquet-go-source/hdfs"
+	"github.com/xitongsys/parquet-go/writer"
 )
 
+const apiURL = "https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT"
+
 func main() {
-	client, err := hdfs.NewClient(hdfs.ClientOptions{
-		Addresses: []string{"localhost:8020"},
-		User:      "hadoop",
-	})
-	if err != nil {
-		log.Fatal(err)
+	ctx, cancel := context.WithCancel(context.Background())
+	go process(ctx)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case s := <-interrupt:
+		log.Println("application got signal", s.String())
+		cancel()
+		time.Sleep(3 * time.Second) // таумаут для завершения работы приложения
 	}
-	defer client.Close()
+}
 
-	// create dir
-	// if err = client.Mkdir("/user", 0777); err != nil {
-	// 	log.Fatal(err)
-	// }
+type Record struct {
+	ID         string  `parquet:"name=id, type=BYTE_ARRAY"`
+	Currency   string  `parquet:"name=currency, type=BYTE_ARRAY"`
+	PriceFloat float64 `parquet:"name=price_float, type=DOUBLE"`
+	PriceInt   int64   `parquet:"name=price_int, type=INT64"`
+	Price      string  `json:"price" parquet:"name=price_str, type=BYTE_ARRAY"`
+	Timestamp  int64   `parquet:"name=timestamp, type=INT64, convertedtype=TIMESTAMP_MILLIS"`
+}
 
-	files, err := client.ReadDir("/user")
+func process(ctx context.Context) {
+	t := time.NewTicker(3 * time.Second)
+
+	fw, err := hdfs.NewHdfsFileWriter([]string{"localhost:8020"}, "hadoop", "output.parquet")
 	if err != nil {
-		log.Fatalf("Failed to list directory: %v", err)
+		log.Println("Can't create file", err)
+		return
 	}
 
-	for _, file := range files {
-		log.Printf("Name: %s, Size: %d bytes, Directory: %v\n", file.Name(), file.Size(), file.IsDir())
-		content, err := client.ReadFile(fmt.Sprintf("/user/%s", file.Name()))
-		if err != nil {
-			log.Println("failed to read file", file.Name(), err)
-			continue
+	//write
+	pw, err := writer.NewParquetWriter(fw, new(Record), 4)
+	if err != nil {
+		log.Println("Can't create parquet writer", err)
+		return
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("process stopped")
+			pw.WriteStop()
+			fw.Close()
+			t.Stop()
+			return
+		case <-t.C:
+			var r = Record{
+				ID:        uuid.NewString(),
+				Currency:  "BTC",
+				Timestamp: time.Now().Unix(),
+			}
+
+			resp, err := http.Get(apiURL)
+			if err != nil {
+				log.Println("failed to get reponse", err)
+				continue
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != 200 {
+				log.Println("failed to get successful response", resp.StatusCode)
+				continue
+			}
+
+			respBody, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Println("failed to read reponse body", err)
+				continue
+			}
+
+			if err = json.Unmarshal(respBody, &r); err != nil {
+				log.Println("failed to unmarshal", err)
+				continue
+			}
+
+			r.PriceFloat, err = strconv.ParseFloat(r.Price, 64)
+			if err != nil {
+				log.Println("failed to parse float", err)
+				continue
+			}
+
+			r.PriceInt = int64(r.PriceFloat * math.Pow10(8))
+
+			if err = pw.Write(r); err != nil {
+				log.Println("failed to write to parquet file")
+				continue
+			}
+
+			log.Println("successful write", r)
 		}
-		log.Printf("%s\n", string(content))
 	}
-
-	// dir, err := client.ReadDir("user")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// for _, f := range dir {
-	// 	log.Println(f.Name())
-	// }
-	//
-	// file, err := client.Open("/user/text.txt")
-	// if err != nil {
-	// 	var pathErr *os.PathError
-	// 	if errors.As(err, &pathErr) {
-	// 		log.Println("file /user/text.txt is empty", err)
-	// 	} else {
-	// 		log.Fatal("failed to open file", err)
-	// 	}
-	// }
-	//
-	// _ = file
-	//
-	// file, err = client.Open("/user/text2.txt")
-	// if err != nil {
-	// 	var pathErr *os.PathError
-	// 	if errors.As(err, &pathErr) {
-	// 		log.Println("file /user/text2.txt is empty", err)
-	// 	} else {
-	// 		log.Fatal("failed to open file", err)
-	// 	}
-	// }
 }
