dc2974c94930c51119ffb559ac7259bf5d6bde53
Roman Chechyotkin <romanchechyotkin@gmail.com>
2025-03-13 23:57:34 +0300

    html extract

--- html-extractor/go.mod
+++ html-extractor/go.mod
@@ -4,5 +4,9 @@ go 1.24.0
 
 require (
 	github.com/huantt/plaintext-extractor v1.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
 	golang.org/x/net v0.12.0 // indirect
+	jaytaylor.com/html2text v0.0.0-20230321000545-74c2419ad056 // indirect
 )
--- html-extractor/main.go
+++ html-extractor/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	extractor "github.com/huantt/plaintext-extractor"
+	"jaytaylor.com/html2text"
 )
 
 func main() {
@@ -23,4 +24,12 @@ func main() {
 	}
 	fmt.Println(*output)
 
+	a = "<html><body> <h1>вот и html показываем</h1> <p>Вам пришло письмо c внутреннего сервиса <a href=\"https://www.ozon.ru/product/vlazhnyy-korm-dlya-sobak-cesar-adult-s-govyadinoy-i-ovoshchami-kusochki-v-souse-28-h-85-g-181790517/?avtc=1&avte=1&avts=1675334014&sh=sSqPgMOn3Q&comm=41qfLA0GQzIckIlnlY9002WSgM40000004000000000bT1OrShRoTgLtCNxuCxKunAJqSZOriRAr7BxbndLoC5HbmdBsS5Obm5AtmNQbncJpSZSum5AqmVLuiRFbmZSrTdEoSxxrmAJqTlPrSdEqSAJtyRPrTlPpiQOe2REbjwRbmsJcjwNdPAMdj4TbPZxtDhzfj4ConpQpjQN9C5St7cZcjoTdjcPd30Nd2pPq3RPkT5gpQRfrzdh\">КАКАЯ ТО ССЫЛКА</a></p> <p>С уважением,<br> Команда COMMS.</p> <img src=\"https://api.ozon.ru/web-api.comms/mo/12tZfU0GQzIckIlnlY9002WSgM40000004000000000.gif\" width=\"1\" height=\"1\"></body></html>"
+
+	text, err := html2text.FromString(a, html2text.Options{PrettyTables: true})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(text)
 }
